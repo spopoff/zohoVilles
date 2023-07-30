fe36a554800a32cdf2049419448f64e28417fa9c
spopoff <sgpopoff@yahoo.com>
2023-07-30 13:57:53 +0200

    kali test +++++++

--- contacts.js
+++ contacts.js
@@ -8673,7 +8673,8 @@ var cnt851826000046105945 = new Contact("MIRANDA PAZ","Ana Maria","Mme Ana Maria
 contacts.set(851826000046105945, cnt851826000046105945);
 var cnt851826000046105946 = new Contact("MIREAUX","Elsa","Mme Elsa MIREAUX","elsa.mireaux@ville-epernay.fr",{name:"Ville d'Epernay", id:"851826000053862001"},{name:"Albane REAL", id:"851826000000346011"},"Membre cotisant",[],"851826000046105946");
 contacts.set(851826000046105946, cnt851826000046105946);
-var cnt851826000000117650 = new Contact("RAHMANI","Xavier","M. Xavier RAHMANI","x.rahmani@orange.fr,xrahmani@ville-saintdie.fr",{name:"Ville de Saint-Dié-des-Vosges", id:"851826000000789055"},{name:"en Mouvement Centre-Ville", id:"851826000000064003"},"",[],"851826000000117650");
+var cnt851826000000117650 = new Contact("RAHMANI","Xavier","M. Xavier RAHMANI","x.rahmani@orange.fr
+xrahmani@ville-saintdie.fr",{name:"Ville de Saint-Dié-des-Vosges", id:"851826000000789055"},{name:"en Mouvement Centre-Ville", id:"851826000000064003"},"",[],"851826000000117650");
 contacts.set(851826000000117650, cnt851826000000117650);
 var cnt851826000046105943 = new Contact("MIMEUR","Amanda","Mme Amanda MIMEUR","amanda.mimeur@ville-nevers.fr",{name:"Ville de Nevers", id:"851826000050440072"},{name:"Albane REAL", id:"851826000000346011"},"Membre cotisant",[],"851826000046105943");
 contacts.set(851826000046105943, cnt851826000046105943);
@@ -18251,7 +18252,8 @@ var cnt851826000000110967 = new Contact("DE CHILLY","Alain","M. Alain DE CHILLY"
 contacts.set(851826000000110967, cnt851826000000110967);
 var cnt851826000051610129 = new Contact("Le Torrec","Mathieu","Mathieu Le Torrec","null",{name:"CA Mont-Saint-Michel-Normandie", id:"851826000050528168"},{name:"Clément BRIHAT", id:"851826000016139058"},"",[],"851826000051610129");
 contacts.set(851826000051610129, cnt851826000051610129);
-var cnt851826000000110968 = new Contact("PITTET","Lise","Mme Lise PITTET","l.pittet@incite-bordeaux.fr",{name:"inCité Bordeaux Métropole Territoires", id:"851826000012682033"},{name:"Gwenaëlle Airiau", id:"851826000000064011"},"",[],"851826000000110968");
+var cnt851826000000110968 = new Contact("PITTET","Lise","Mme Lise PITTET","L.PITTET@INCITE-BORDEAUX.FR
+l.pittet@incite-bordeaux.fr",{name:"inCité Bordeaux Métropole Territoires", id:"851826000012682033"},{name:"Gwenaëlle Airiau", id:"851826000000064011"},"",[],"851826000000110968");
 contacts.set(851826000000110968, cnt851826000000110968);
 var cnt851826000051610122 = new Contact("Le Pissart","Benjamin","Benjamin Le Pissart","null",{name:"CA Mont-Saint-Michel-Normandie", id:"851826000050528168"},{name:"Clément BRIHAT", id:"851826000016139058"},"",[],"851826000051610122");
 contacts.set(851826000051610122, cnt851826000051610122);
