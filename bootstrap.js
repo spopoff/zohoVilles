655f5715830b494ca3a3e5038923aeea60856b73
spopoff <sgpopoff@yahoo.com>
2023-08-24 18:29:31 +0200

    param gqlCVM

--- bootstrap.js
+++ bootstrap.js
@@ -51,7 +51,7 @@ var simContacts = [];
 var contacts = [];
 var accounts = [];
 const zohoCVM = "https://crm.zoho.com/crm/org26538990/";
-const spoCVM = "http://localhost:8099/graphql";
+const gqlCVM = "http://ebcvm3-env.eba-ysawcnrp.eu-west-1.elasticbeanstalk.com/graphql";
 
 class DataChart{
     constructor(code, type, label){
@@ -302,41 +302,5 @@ if(!window.HashChangeEvent)(function(){
 document.addEventListener("DOMContentLoaded", () => {
   simLeads.sort((a, b) => a.id2.localeCompare(b.id2));
   simContacts.sort((a, b) => a.id2.localeCompare(b.id2));
-});
-;(function() {
-    var pushState = history.pushState;
-    var replaceState = history.replaceState;
-
-    history.pushState = function() {
-        pushState.apply(history, arguments);
-        window.dispatchEvent(new Event('pushstate'));
-        window.dispatchEvent(new Event('locationchange'));
-    };
-
-    history.replaceState = function() {
-        replaceState.apply(history, arguments);
-        window.dispatchEvent(new Event('replacestate'));
-        window.dispatchEvent(new Event('locationchange'));
-    };
-
-    window.addEventListener('popstate', function() {
-        window.dispatchEvent(new Event('locationchange'));
-    });
-})();
-
-window.addEventListener('locationchange', function(){
-    console.log('onlocationchange event occurred!');
-    var prms = getQueryString(location.href);
-    if(prms.pkl !== undefined){
-        getLeadInfo(prms.pkl);
-    }
-    if(prms.pkc !== undefined){
-        getContactInfo(prms.pkc);
-    }
-    if(prms.pka !== undefined){
-        getAccountInfo(prms.pka);
-    }
-    if(prms.fk !== undefined){
-        fusionLink(prms.fk);
-    }
+  document.getElementById("gqlCVM").value = gqlCVM;
 });
